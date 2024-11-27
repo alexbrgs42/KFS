dfbe29566e33cad49db99ebe2d9c5a57509c7062
iaschnei <iaschnei@student.42.fr>
2024-11-27 15:01:57 +0100

    Added colors to printk output

--- src/LIBS/LIBC/printk.c
+++ src/LIBS/LIBC/printk.c
@@ -6,23 +6,31 @@ int printk(uint16_t log_level, const char *string) {
     switch (log_level) {
 
         case 1:
+            terminal_set_color(VGA_COLOR_RED);
             terminal_write_buffer("CRITICAL ERROR: ");
             terminal_write_buffer(string);
+            terminal_set_color(VGA_COLOR_WHITE);
             break ;
         
         case 2:
+            terminal_set_color(VGA_COLOR_LIGHT_RED);
             terminal_write_buffer("ERROR: ");
             terminal_write_buffer(string);
+            terminal_set_color(VGA_COLOR_WHITE);
             break ;
 
         case 3:
+            terminal_set_color(VGA_COLOR_LIGHT_MAGENTA);
             terminal_write_buffer("WARNING: ");
             terminal_write_buffer(string);
+            terminal_set_color(VGA_COLOR_WHITE);
             break ;
 
         case 4:
+            terminal_set_color(VGA_COLOR_CYAN);
             terminal_write_buffer("INFO: ");
             terminal_write_buffer(string);
+            terminal_set_color(VGA_COLOR_WHITE);
             break ;
 
         default:
